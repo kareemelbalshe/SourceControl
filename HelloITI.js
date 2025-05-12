bd2ede72ccf4b6be00aea8c36e156d5efd8000b6
kareemelbalshe <kareemelbalshe1234@gmail.com>
2025-05-12 12:35:49 +0300

    Add printHelloGit to HelloITI

--- HelloITI.js
+++ HelloITI.js
@@ -2,7 +2,12 @@ class HelloITI {
   printHelloTI() {
     console.log("Hello ITI");
   }
+
+  printHelloGit() {
+    console.log("Hello Git");
+  }
 }
 
 const iti = new HelloITI();
 iti.printHelloTI();
+iti.printHelloGit();
